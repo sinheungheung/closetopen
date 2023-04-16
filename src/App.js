d22bf7aa1b67b4a8acc8e9c6b390f0ae1b16932d
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-04-16 23:13:57 +0900

    StartPage 라우터 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import StartPage from './page/StartPage';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path='/start' element={<StartPage/>}/>
+      </Routes>
     </div>
   );
 }
