2edee1f86005f42baee39ffe87e8e71ea75ac114
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-04-16 23:14:13 +0900

    StartPage 이미지 위치 조정

---
+++ src/page/StartPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css';
+
+const StartPage = () => {
+  return (
+    <div className="start-page" >
+      <div className='start-logo'></div>
+      <div className='start-btn'></div>
+    </div>
+  )
+}
+
+export default StartPage
