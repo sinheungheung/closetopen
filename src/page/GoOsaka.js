df81ac67486f19d534c883618eefd77865796055
sinheungheung <s2120@e-mirim.hs.kr>
2023-05-11 10:08:53 +0900

    오사카의 모든 이미지 추가하기(위치 수정 필요)

--- src/page/GoOsaka.js
+++ src/page/GoOsaka.js
@@ -34,6 +34,15 @@ const GoOsaka = () => {
     <div class="parent">
       <div className="CheckOsaka" onClick={goGameStart}></div>
       <div class="OSAKA"></div>
+      <div class="OSAKA-ex"></div>
+      <div class="CloseButton"></div>
+      <div class="SomeArea"></div>
+      <div class="Harukas-img"></div>
+      <div class="Harukas-kr"></div>
+      <div class="Glycosan-img"></div>
+      <div class="Glycosan-kr"></div>
+      <div class="Zoo-img"></div>
+      <div class="Zoo-kr"></div>
       </div>
 
   );
