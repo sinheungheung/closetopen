32db000408a6bc62152cd47e02f8242075f6ac5c
sinheungheung <s2120@e-mirim.hs.kr>
2023-05-15 20:55:22 +0900

    Sapporo X버튼과 이미지 버튼에 기능 추가 완료

--- src/page/GoSapporo.js
+++ src/page/GoSapporo.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import '../App.css';
 import { useNavigate } from 'react-router-dom';
 
 const GoSapporo = () => {
 const movePage = useNavigate();
+const [hover, setHover] = useState(false);
 
 function goGameStart(){
   movePage('/GameStart');
+}
+const handleMouseOver = () => {
+  setHover(true);
+};
+
+const handleMouseOut = () => {
+  setHover(false);
+};
+
+// X를 누르면 Button페이지로 이동
+const navigate = useNavigate();
+
+function handleClick() {
+  navigate('/Button');
 }
 
 return (
@@ -14,15 +29,15 @@ return (
       <div className="CheckSapporo" onClick={goGameStart}></div>
       <div class="SAPPORO"></div>
       <div class="SAPPORO-ex"></div>
-      <div class="CloseButton"></div>
       <div class="SomeArea"></div>
       <div class="IceFestival-img"></div>
-      <div class="IceFestival-kr"></div>
       <div class="ArtMuseum-img"></div>
-      <div class="ArtMuseum-kr"></div>
       <div class="ShoppingStore-img"></div>
+      <div class="CloseButton" onClick={handleClick}></div>
+      <div class="IceFestival-kr"></div>
+      <div class="ArtMuseum-kr"></div>
       <div class="ShoppingStore-kr"></div>
-      <div class="NextButton"></div>
+      {/* <div class="NextButton"></div> */}
       </div>
 
   );
