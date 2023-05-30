061f333a60b4ed4c94c37e86c0bb435ae924b76d
sinheungheung <s2120@e-mirim.hs.kr>
2023-05-31 01:39:13 +0900

    드래그 앤 드롭과 마우스 이벤트로 캐릭터 위에 옷 드래그하여 입히기 기능 구현 시도 중

--- src/page/ChangeCloth.js
+++ src/page/ChangeCloth.js
@@ -1,77 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../css/ChangeCloth.css';
 import { useNavigate } from 'react-router-dom';
 
 const ChangeCloth = () => {
-const movePage = useNavigate();
-const [hover, setHover] = useState(false);
+  const movePage = useNavigate();
+  const [hover, setHover] = useState(false);
 
-// function (){
-//   movePage('/');
-// }
-const handleMouseOver = () => {
-  setHover(true);
-};
-
-const handleMouseOut = () => {
-  setHover(false);
-};
-var TopClothes_img = ['../images/ShortTshirt-Empty.png',
-                      '../images/Knit-Empty.png',
-                      '../images/WindBreaker-Empty.png',
-                      '../images/Short-Empty.png'];
-var TopClothes_index = 0;
-
-let draggedItem = null;
-
-function drag(){
-  draggedItem = this;
-  console.log(draggedItem.src);
-}
-function dragenter(){
-  document.getElementById('chara-clo').src=TopClothes_img[TopClothes_index];
-}
-function changeOnFooter(){
-  document.getElementById('onoff').style.display = "block";
-}
-function changeOffFooter(){
-  document.getElementById('onoff').style.display = "none";
-}
-
-function upclothesBotton(){
-  if(TopClothes_index>=3){
-      TopClothes_index=0;
-  }else{
-      TopClothes_index++;
-  }
-  document.getElementById('chara-clothes').src = TopClothes_img[TopClothes_index];
-}
-function downclothesBotton(){
-  if(TopClothes_index<=0){
-      TopClothes_index=3;
-  }else{
-      TopClothes_index--;
-  }
-  document.getElementById('chara-clothes').src = TopClothes_img[TopClothes_index];
-}
-
-
-    return (
-      <div className="MainGame">
-         <div className="OpenCloset"></div>
-         <div className="Character"></div> 
-        {/* 상의 이미지 */}
-         <div className="ShortTshirt-Empty"></div>  
-         <div className="Knit-Empty"></div>         
-         <div className="WindBreaker-Empty"></div>  
-         <div className="Short-Empty"></div>  
-         {/* 하의 이미지 */}
-         <div className="LongSkirt"></div>  
-         <div className="Slacks"></div>         
-         <div className="JoggerPants"></div>  
-         <div className="CargoPants"></div>  
-      </div>
-    );
+  const handleMouseOver = () => {
+    setHover(true);
   };
-  
-  export default ChangeCloth;
+
+  const handleMouseOut = () => {
+    setHover(false);
+  };
+
+  const handleDragStart = (event, itemId) => {
+    event.dataTransfer.setData('itemId', itemId);
+  };
+  // 드래그 시작 시 호출되는 이벤트 핸들러
+  // ID는 드롭 이벤트에서 해당 요소를 식별하는 데 사용함
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+  // 드래그 중인 요소가 적절한 드롭 대상 위로 들어갔을 때 호출됨
+  // 드롭 허용하도록 설정
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    // 기본 동작을 방지함
+    const itemId = event.dataTransfer.getData('itemId');
+    // 드래그된 요소의 ID를 가져오고 요소를 찾음
+    const item = document.getElementById(itemId);
+    event.target.appendChild(item);
+    // 요소를 드롭 대상에 추가함
+  };
+
+  return (
+    <div className="MainGame">
+      <div className="OpenCloset"></div>
+      <div className="Character"></div>
+      {/* 상의 이미지 */}
+      <div className="ShortTshirt-Empty" draggable="true" onDragStart={(event) => handleDragStart(event, 'shortTshirt')}></div>
+      <div className="Knit-Empty" draggable="true" onDragStart={(event) => handleDragStart(event, 'knit')}></div>
+      <div className="WindBreaker-Empty" draggable="true" onDragStart={(event) => handleDragStart(event, 'windBreaker')}></div>
+      <div className="Short-Empty" draggable="true" onDragStart={(event) => handleDragStart(event, 'short')}></div>
+      {/* 하의 이미지 */}
+      <div className="LongSkirt" draggable="true" onDragStart={(event) => handleDragStart(event, 'longSkirt')}></div>
+      <div className="Slacks" draggable="true" onDragStart={(event) => handleDragStart(event, 'slacks')}></div>
+      <div className="JoggerPants" draggable="true" onDragStart={(event) => handleDragStart(event, 'joggerPants')}></div>
+      <div className="CargoPants" draggable="true" onDragStart={(event) => handleDragStart(event, 'cargoPants')}></div>
+
+      {/* 캐릭터 영역 */}
+      <div className="CharacterArea" onDragOver={handleDragOver} onDrop={handleDrop}></div>
+    </div>
+  );
+};
+
+export default ChangeCloth;
