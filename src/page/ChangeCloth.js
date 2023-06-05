bc06e76ebe4f12abd8c8669f6c2d9f12009e0ae4
sinheungheung <s2120@e-mirim.hs.kr>
2023-06-05 22:33:41 +0900

    캐릭터 위에 옷 입히기

--- src/page/ChangeCloth.js
+++ src/page/ChangeCloth.js
@@ -3,11 +3,12 @@ import '../css/ChangeCloth.css';
 import { useNavigate } from 'react-router-dom';
 
 
+
 // 상의 이미지
-import ShortTshirtEmpty from '../images/ShortTshirt-Empty.png';
+import ShortTshirt from '../images/ShortTshirt-Empty.png';
 import Knit from '../images/Knit-Empty.png';
 import WindBreaker from '../images/WindBreaker-Empty.png';
-import ShortEmpty from '../images/Short-Empty.png';
+import Short from '../images/Short-Empty.png';
 import Cardigan from '../images/Cardigan-Empty.png';
 import Blouse from '../images/Blouse-Empty.png';
 
@@ -27,38 +28,8 @@ import Kimono from '../images/Kimono-Empty.png';
 const ChangeCloth = () => {
   const movePage = useNavigate();
 
-  // 드래그 기능
-  const [ShortTshirtEmptyPosition, setShirtPosition] = useState({ x: 0, y: 0 });
-  const [KnitPosition, setKnitPosition] = useState({ x: 0, y: 0 });
-  const [WindBreakerPosition, setWindBreakerPosition] = useState({ x: 0, y: 0 });
-// ---------------------------------------------------
-  const handleDragStart = (event, item) => {
-    // 드래그 시작 시 옷의 위치를 기록
-    const { clientX, clientY } = event;
-    const offsetX = clientX - event.target.offsetLeft;
-    const offsetY = clientY - event.target.offsetTop;
 
-    if (item === 'shirt') {
-      setShirtPosition({ x: offsetX, y: offsetY });
-    } else if (item === 'pants') {
-      setPantsPosition({ x: offsetX, y: offsetY });
-    }
-  };
-
-  const handleDrag = (event, item) => {
-    // 드래그 중인 상태에서 옷의 위치를 업데이트
-    const { clientX, clientY } = event;
-    const offsetX = clientX - event.target.offsetLeft;
-    const offsetY = clientY - event.target.offsetTop;
-
-    if (item === 'ShortTshirt') {
-      ShortTshirtEmptyPosition({ x: offsetX, y: offsetY });
-    } else if (item === 'Knit') {
-      KnitPosition({ x: offsetX, y: offsetY });
-    }
-  };
-// -------------------------------------------------
-  const topClothes = [ShortTshirtEmpty, Knit, WindBreaker, ShortEmpty, Cardigan, Blouse];
+  const topClothes = [ShortTshirt, Knit, WindBreaker, Short, Cardigan, Blouse];
   const bottomClothes = [LongSkirt, Slacks, JoggerPants, CargoPants, Jeans, Skirt];
   const setClothes =[Onepiece, Tteokbokkicoat, Kimono];
 
@@ -101,43 +72,14 @@ const ChangeCloth = () => {
         <img src={topClothes[currentTopIndex]} alt="상의 이미지" />
         <img src={require('../images/RightButton.png')} onClick={nextTopSlide} alt="오른쪽" className="RightButton" /> 
     
-    
-      {/* -------------------------------- */}
-       
-      <img
-          src={ShirtImage}
-          alt="Shirt"
-          draggable="true"
-          onDragStart={(event) => handleDragStart(event, 'shirt')}
-          onDrag={(event) => handleDrag(event, 'shirt')}
-          style={{ top: shirtPosition.y, left: shirtPosition.x }}
-        />
-
-        
-     
-       {/* -------------------------------- */}
-    
-    
       </div>
 
       {/* 하의 이미지 */}
       <div className="TopSlider">
         <img src={require('../images/LeftButton.png')} onClick={previousTopSlide} alt="왼쪽" className="LeftButton" /> 
         <img src={bottomClothes[currentTopIndex]} alt="하의 이미지" />
-        <img src={require('../images/RightButton.png')} onClick={nextTopSlide} alt="오른쪽" className="RightButton" /> 
-        {/* -------------------------------- */}
-       
-        <img
-          src={PantsImage}
-          alt="Pants"
-          draggable="true"
-          onDragStart={(event) => handleDragStart(event, 'pants')}
-          onDrag={(event) => handleDrag(event, 'pants')}
-          style={{ top: pantsPosition.y, left: pantsPosition.x }}
-        />
-     
-       {/* -------------------------------- */}
-      
+        <img src={require('../images/RightButton.png')} onClick={nextTopSlide} alt="오른쪽" className="RightButton" />
+
       </div>
 
 
