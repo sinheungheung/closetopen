682b99bb83c6c817e7128daab430eea167a5cddb
sinheungheung <s2120@e-mirim.hs.kr>
2023-06-14 11:46:55 +0900

    스토리텔링 한문장씩 아래로 띄어쓰기 구현

--- src/page/Storytelling1.js
+++ src/page/Storytelling1.js
@@ -8,29 +8,35 @@ function Storytelling1() {
   const place = location.state.value;
 
   const tokyo_lines = [
-    '오늘은 도쿄의 메이지 신궁에 갈거야!',
-    '일본의 역서가 담긴 곳인 만큼 전통적인 옷을 입으면 좋을 것 같네~',
+    '오늘은 도쿄의 메이지 신궁에 갈거야!\n',
+    '일본의 역서가 담긴 곳인 만큼 전통적인 옷을 입으면 좋을 것 같네~\n',
     '무슨 옷을 입어볼까?',
   ];
 
   const osaka_lines = [
-    '오늘은 오사카 도톤보리의 글리코상을 보러 갈거야!',
-    '유행하는 패션들이 가득한 곳이고, 여러 놀거리가 많다고 하니 편하면서 트렌디한 느낌을 입고 싶어!',
+    '오늘은 오사카 도톤보리의 글리코상을 보러 갈거야!\n',
+    '유행하는 패션들이 가득한 곳이고, 여러 놀거리가 많다고 하니 편하면서 트렌디한 느낌을 입고 싶어!\n',
     '무슨 옷을 입어볼까?',
   ];
 
   const sapporo_lines = [
-    '오늘은 삿포로의 오도리 공원 얼음축제에 갈거야!', 
+    '오늘은 삿포로의 오도리 공원 얼음축제에 갈거야!',
     '추운 곳으로 가니까 따뜻하고 귀엽게 입으면 좋을 것 같아!',
     '무슨 옷을 입어볼까?',
   ];
 
   const [line, setLine] = useState(0);
   const [displayText, setDisplayText] = useState('');
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
   const changeLine = () => {
-    setLine(line + 1);
-    if (line === 2) navigate('/ChangeCloth');
+    if (line < 2) {
+      setLine(line + 1);
+      setDisplayText('');
+      setButtonDisabled(true);
+    } else {
+      navigate('/ChangeCloth');
+    }
   };
 
   const pl =
@@ -46,17 +52,16 @@ function Storytelling1() {
 
     const startTyping = () => {
       intervalId = setInterval(() => {
-        if (currentIndex < pl.length ) {
-          setDisplayText(prevText => {
-           
+        if (currentIndex < pl.length) {
+          setDisplayText((prevText) => {
             const currentChar = pl[currentIndex] === ' ' ? '\u00A0' : pl[currentIndex];
-            if(currentChar == undefined) return prevText ;
-            //console.log(prevText + currentChar);
+            if (currentChar === undefined) return prevText;
             return prevText + currentChar;
           });
           currentIndex++;
         } else {
           clearInterval(intervalId);
+          setButtonDisabled(false);
         }
       }, 100);
     };
@@ -70,17 +75,18 @@ function Storytelling1() {
 
   return (
     <div>
-      <div className='main'>
-        <img className='character' src={`/images/character.png`} alt='' />
-        <img className='closet' src={`/images/closet-close.png`} alt='' />
+      <div className="main">
+        <img className="character" src={`/images/character.png`} alt="" />
+        <img className="closet" src={`/images/closet-close.png`} alt="" />
         <div>
-          <img className='textbox' src={`/images/textbox.png`} alt='' />
-          <div className='line'>{displayText}</div>
+          <img className="textbox" src={`/images/textbox.png`} alt="" />
+          <div className="line">{displayText}</div>
           <img
-            className='nextbtn'
+            className="nextbtn"
             src={`/images/storybtn.png`}
-            alt=''
+            alt=""
             onClick={changeLine}
+            disabled={buttonDisabled}
           />
         </div>
       </div>
