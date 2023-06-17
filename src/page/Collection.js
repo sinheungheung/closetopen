9ab564feeefe287ece54f3f006737c5f744a94f9
sinheungheung <s2120@e-mirim.hs.kr>
2023-06-18 02:18:59 +0900

    왼쪽과 오른쪽 버튼 누를때마다 CollectBox가 무한대로 생성

--- src/page/Collection.js
+++ src/page/Collection.js
@@ -1,30 +1,49 @@
 import React, { useState } from "react";
-import '../css/Collection.css';
-import { useNavigate } from 'react-router-dom';
+import "../css/Collection.css";
+import { useNavigate } from "react-router-dom";
 
 const Collection = () => {
   const navigate = useNavigate();
   const [hover, setHover] = useState(false);
   const [position, setPosition] = useState(0);
+  const [boxCount, setBoxCount] = useState(1); // Track the number of CollectBox elements
 
   // X를 누르면 /Storytelling2페이지로 이동
   function handleClick() {
-    navigate('/secondstorytelling');
+    navigate("/secondstorytelling");
   }
 
   function handleLeftClick() {
-    setPosition(prevPosition => prevPosition - 1400); // Adjust the value according to your needs
+    setPosition((prevPosition) => prevPosition - 1400); // Adjust the value according to your needs
+    setBoxCount((prevCount) => prevCount + 1); // Increment the box count
   }
 
   function handleRightClick() {
-    setPosition(prevPosition => prevPosition + 1400); // Adjust the value according to your needs
+    setPosition((prevPosition) => prevPosition + 1400); // Adjust the value according to your needs
+    setBoxCount((prevCount) => prevCount + 1); // Increment the box count
   }
+
+  // Generate CollectBox elements based on boxCount
+  const generateCollectBoxes = () => {
+    const boxes = [];
+    for (let i = 0; i < boxCount; i++) {
+      const leftPosition = 300 + i * 1550; // Adjust the left position according to your needs
+      boxes.push(
+        <div
+          className={`CollectBox CollectBox${i + 1}`}
+          key={i + 1}
+          style={{ transform: `translateX(${position}px)`, left: `${leftPosition}px` }}
+        ></div>
+      );
+    }
+    return boxes;
+  };
 
   return (
     <div className="main">
       <div className="collection">
         <div className="CloseButton" onClick={handleClick}></div>
-        <div className="CollectBox" style={{ transform: `translateX(${position}px)` }}></div>
+        {generateCollectBoxes()}
         <div className="CollectLeft" onClick={handleLeftClick}></div>
         <div className="CollectRight" onClick={handleRightClick}></div>
       </div>
