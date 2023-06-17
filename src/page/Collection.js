a9c884dfd48a1e3d38f1571f8ad9ad42d5b5a959
sinheungheung <s2120@e-mirim.hs.kr>
2023-06-18 01:53:19 +0900

    컬렉션 왼쪽 버튼 오른쪽 버튼 누르면 화면 움직이기

--- src/page/Collection.js
+++ src/page/Collection.js
@@ -1,27 +1,48 @@
 import React, { useState } from "react";
-import '../css/Collection.css';
-import { useNavigate } from 'react-router-dom';
-
+import "../css/Collection.css";
+import { useNavigate } from "react-router-dom";
 
 const Collection = () => {
   const navigate = useNavigate();
+  const [hover, setHover] = useState(false);
+  const [translateX, setTranslateX] = useState(0);
 
   // X를 누르면 /Storytelling2페이지로 이동
   function handleClick() {
-    navigate('/Storytelling2');
+    navigate("/secondstorytelling");
+  }
+
+  function handleRightButtonClick() {
+    setTranslateX((prevTranslateX) => prevTranslateX + 100);
+  }
+
+  function handleLeftButtonClick() {
+    setTranslateX((prevTranslateX) => prevTranslateX - 100);
   }
 
   return (
-    <div class="main">
-        <div class="CloseButton" onClick={handleClick}></div>
-        <div class="CollectBox"></div> 
-        <div class="CollectLeft"></div> 
-        <div class="CollectRight"></div> 
-   
+    <div className="main">
+      <div className="collection">
+        <div className="CloseButton" onClick={handleClick}></div>
+        <div className="CollectContainer">
+          <div
+            className="CollectBox"
+            style={{ transform: `translateX(${translateX}%)` }}
+          ></div>
+          <div
+            className="CollectBox2"
+            style={{ transform: `translateX(${translateX}%)` }}
+          ></div>
+          <div
+            className="CollectBox3"
+            style={{ transform: `translateX(${translateX}%)` }}
+          ></div>
+        </div>
+        <div className="CollectLeft" onClick={handleLeftButtonClick}></div>
+        <div className="CollectRight" onClick={handleRightButtonClick}></div>
       </div>
-      
+    </div>
   );
-  
 };
 
 export default Collection;
