414fef4eae3e54ca311105038103827841d7ec0c
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-06-17 16:03:45 +0900

    옷입히기 구현 완료

--- src/page/ChangeCloth.js
+++ src/page/ChangeCloth.js
@@ -6,25 +6,26 @@ import axios from "axios";
 
 // // 빈 이미지
 import Empty from '../images/Empty.png';
-import EmptyTop from '../images/EmptyTop.png'; //상의 초기화
-// import EmptyBottom from '../images/EmptyBottom.png'; //하의 초기화
+import EmptyTop from '../images/cloths/top.png'; //상의 초기화
+import EmptyTop_c from '../images/cloths/top-c.png';
 import EmptyBottom from '../images/cloths/bottom.png'; //하의 초기화
 
 // 상의 이미지
-import ShortTshirt from '../images/ShortTshirt-Empty.png';
-import Knit from '../images/Knit-Empty.png';
-import WindBreaker from '../images/WindBreaker-Empty.png';
-import Short from '../images/Short-Empty.png';
-import Cardigan from '../images/Cardigan-Empty.png';
-import Blouse from '../images/Blouse-Empty.png';
+import ShortTshirt from '../images/cloths/shirt.png';
+import Knit from '../images/cloths/knit.png';
+import WindBreaker from '../images/cloths/windbreaker.png';
+import Shirt from '../images/cloths/shirt.png';
+import Cardigan from '../images/cloths/cardigan.png';
+import Blouse from '../images/cloths/blouse.png';
+
+import ShortTshirt_c from '../images/cloths/shirt-c.png';
+import Knit_c from '../images/cloths/knit-c.png';
+import WindBreaker_c from '../images/cloths/windbreaker-c.png';
+import Shirt_c from '../images/cloths/shirt-c.png';
+import Cardigan_c from '../images/cloths/cardigan-c.png';
+import Blouse_c from '../images/cloths/blouse-c.png';
 
 // 하의 이미지
-// import LongSkirt from '../images/LongSkirt.png';
-// import Slacks from '../images/Slacks.png';
-// import JoggerPants from '../images/JoggerPants.png';
-// import CargoPants from '../images/CargoPants.png';
-// import Jeans from '../images/Jeans.png';
-// import Skirt from '../images/Skirt.png';
 import LongSkirt from '../images/cloths/longskirt.png';
 import Slacks from '../images/cloths/slacks.png';
 import JoggerPants from '../images/cloths/joggerpants.png';
@@ -34,18 +35,17 @@ import Skirt from '../images/cloths/shortskirt.png';
 
 
 // 세트 이미지
-import Onepiece from '../images/Onepiece-Empty.png';
-import Tteokbokkicoat from '../images/Tteokbokkicoat-Empty.png';
-import Kimono from '../images/Kimono-Empty.png';
+import Onepiece from '../images/cloths/onepiece.png';
+import Tteokbokkicoat from '../images/cloths/coat.png';
+import Kimono from '../images/cloths/kimono.png';
 
 const ChangeCloth = () => {
   const movePage = useNavigate();
 
-  const topClothes = [EmptyTop, ShortTshirt, Knit, WindBreaker, Short, Cardigan, Blouse];
+  const topClothes = [EmptyTop, ShortTshirt, Knit, WindBreaker, Shirt, Cardigan, Blouse];
+  const topClothes_c = [EmptyTop_c, ShortTshirt_c, Knit_c, WindBreaker_c, Shirt_c, Cardigan_c, Blouse_c];
   const bottomClothes = [EmptyBottom, LongSkirt, Slacks, JoggerPants, CargoPants, Jeans, Skirt];
   const setClothes = [Empty, Onepiece, Tteokbokkicoat, Kimono];
-
-  const t_bottomClothes = [EmptyBottom, LongSkirt, Slacks, JoggerPants, CargoPants, Jeans, Skirt];
 
   const [currentTopIndex, setCurrentTopIndex] = useState(0);
   const [currentBottomIndex, setCurrentBottomIndex] = useState(0);
@@ -118,16 +118,11 @@ const ChangeCloth = () => {
     setCurrentSetIndex(index3);
   };
 
-  const Toplocation = [
-    -90,-90,-90,-90,-90,-90,-90
-  ]
-  // (왼쪽버튼 순서) 1번째: 기본 티 2번째: ShortTshirt
-
-  const Bottomlocation = [
-    -50,-30,-30,-30,-30,-100,-30
-    // 1번째: 기본 바지 2번째: LongSkirt
-    
-  ]
+  // 옷 위치 지정
+  const Toplocation = ["27%","27%","27%","29%","28%","29.5%","27%","27%"]
+  const Bottomlocation = ["22%","11.3%","6.5%","7%","7%","6.5%","21%"]
+  const bottomLeftlocation = ["13%","13.1%","13.3%","13%","13.3%","13.3%","13%"]
+  const Setlocation = ["29.5%","29.5%","19%"]
 
   useEffect(() => {
     console.log(values.top);
@@ -141,31 +136,36 @@ const ChangeCloth = () => {
       <div className="CharacterScreen">
         <img className="character" src={`/images/character.png`} alt="" />
         <img
-          style={{ position: 'absolute', left: -240, top: Toplocation[currentTopIndex], width: 1600}}
-          src={!currentSetIndex ? topClothes[currentTopIndex] : EmptyTop}
+          style={{ position: 'absolute', top: Toplocation[currentTopIndex]}}
+          src={!currentSetIndex ? topClothes_c[currentTopIndex] : EmptyTop}
           alt="상의 이미지"
+          className='top-c'
       />
         <img
-          style={{ position: 'absolute', left: -240, bottom: Bottomlocation[(Bottomlocation==6) ?  currentBottomIndex: currentBottomIndex], width: 1600 }}
+          style={{ position: 'absolute',left:bottomLeftlocation[currentBottomIndex], bottom: Bottomlocation[(Bottomlocation==6) ?  currentBottomIndex: currentBottomIndex] }}
           src={!currentSetIndex ? bottomClothes[currentBottomIndex] : EmptyBottom}
           alt="하의 이미지"
+          className='bottom-c'
         />
-        <img style={{ position: 'absolute', left: -240, top: -90, width: 1600 }} src={setClothes[currentSetIndex]} alt="세트 이미지" />
+        <img style={{ position: 'absolute', top: Setlocation[currentTopIndex]}} 
+        src={setClothes[currentSetIndex]} 
+        alt="세트 이미지" 
+        className='set-c'/>
       </div>
 
       {/* 상의 이미지 */}
       <img src={require('../images/LeftButton.png')} onClick={previousTopSlide} alt="왼쪽" className="topLeftButton" />
-      <img src={require('../images/Knit.png')}  className="top" /> {/*임시 이미지*/}
+      <img src={topClothes[currentTopIndex]} alt="상의 이미지" className="top" /> {/*임시 이미지*/}
       <img src={require('../images/RightButton.png')} onClick={nextTopSlide} alt="오른쪽" className="topRightButton" />
 
       {/* 하의 이미지 */}
       <img src={require('../images/LeftButton.png')} onClick={previousBottomSlide} alt="왼쪽" className="bottomLeftButton" />
-      <img src={!currentSetIndex ? t_bottomClothes[currentBottomIndex] : '../images/cloths/longskirt.png'} className="bottom" />
+      <img src={bottomClothes[currentBottomIndex]} className="bottom" />
       <img src={require('../images/RightButton.png')} onClick={nextBottomSlide} alt="오른쪽" className="bottomRightButton" />
 
      {/* 세트 이미지 */}
       <img src={require('../images/LeftButton.png')} onClick={previousSetSlide} alt="왼쪽" className="setLeftButton" />
-      <img src={require('../images/cloths/kimono.png')}  className="set" />
+      <img src={setClothes[currentSetIndex]} className="set" />
       <img src={require('../images/RightButton.png')} onClick={nextSetSlide} alt="오른쪽" className="setRightButton" />
       
       {/* 상의 이미지 */}
