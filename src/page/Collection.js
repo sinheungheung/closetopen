a098f6fafd6a02ce9ef2511f796a8cf4000d772d
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-06-18 23:59:09 +0900

    db에 저장되어 있는 만큼 box출력

--- src/page/Collection.js
+++ src/page/Collection.js
@@ -12,13 +12,13 @@ const Collection = () => {
   const [boxCount, setBoxCount] = useState(3); // 첫 시작화면에 CollectBox 3개 보여주기
   const [value, setValues] = useState([]);
 
-  useEffect(()=>{
-    async function getAll(){
-      try{
+  useEffect(() => {
+    async function getAll() {
+      try {
         const res = await axios.get('getall')
         setValues(res.data);
         setBoxCount(3);
-      } catch(err) {
+      } catch (err) {
         console.log(err);
       }
     };
@@ -33,53 +33,89 @@ const Collection = () => {
   const shoesClothesName = ['shoes1.png', 'shoes2.png', 'shoes3.png', 'shoes4.png', 'shoes5.png', 'shoes6.png'];
 
   // 옷 위치 지정
-  const Toplocation = ["27%","26%","27%","29%","28%","29.5%","27%","27%"]
-  const Bottomlocation = ["22%","11.3%","6.5%","7%","7%","6.5%","21%"]
-  const bottomLeftlocation = ["42%","42.1%","42.3%","42%","42.3%","42.3%","42%"]
-  const setlocation = ["29.5%","29.5%","19%"]
+  const Toplocation = ["27%", "26%", "27%", "29%", "28%", "29.5%", "27%", "27%"]
+  const Bottomlocation = ["24%", "11.3%", "6.5%", "7%", "8.8%", "8.8%", "23%"] //2
+  const bottomLeftlocation = ["33.5%", "42.1%", "42.3%", "42%", "35%", "35%", "34%"] //9
+  const setlocation = ["29.5%", "29.5%", "19%"]
   // 악세사리 위치 지정
-  const accessorieTopLocation = ["4%","1%","8%","4%","1%","4%","4%","4%"]
-  const accessorieLeftLocation = ["41%","41%","40.5%","40.7%","42%","46%"]
+  const accessorieTopLocation = ["4%", "1%", "8%", "4%", "1%", "4%", "4%", "4%"]
+  const accessorieLeftLocation = ["31%", "31%", "30.5%", "30.7%", "32%", "36%"]
   // 신발 위치 지정
-  const shoesTopLocation = ["78%","78.5%","78%","79%","78%","78.5%"]
-
-  // 베열에서 db에서 받은 값의 인덱스 찾기
-  const topIndex = topClothesName.findIndex(item => item === value.top);
-  const bottomIndex = bottomClothesName.findIndex(item => item === value.bottom);
-  const setIndex = setClothesName.findIndex(item => item === value.set);
-  const accessorieIndex = accessorieClothesName.findIndex(item => item === value.accessorie);
-  const shoesIndex = shoesClothesName.findIndex(item => item === value.shoes);
+  const shoesTopLocation = ["78%", "78.5%", "78%", "79%", "78%", "78.5%"]
 
   // X를 누르면 /Storytelling2페이지로 이동
   function handleClick() {
-    navigate("/secondstorytelling", { state : { value : place}});
+    navigate("/secondstorytelling", { state: { value: place } });
   }
 
   function handleLeftClick() {
+    console.log(value);
     setPosition((prevPosition) => prevPosition - 1400);
   }
 
   function handleRightClick() {
-    setPosition((prevPosition) => prevPosition + 1400);
-    setBoxCount((prevCount) => prevCount + 3);
+    if(value.length > 0){
+      setPosition((prevPosition) => prevPosition + 1400);
+      setBoxCount((prevCount) => prevCount + 1);
+    }
   }
 
   const generateCollectBoxes = () => {
-    if(value.length > 0){
+    if (value.length > 0) {
       const boxes = [];
-    console.log(value[0].top)
-    for (let i = 0; i < boxCount; i++) {
-      const leftPosition = 300 + (i - Math.floor(position / 1400)) * 1550;
-      boxes.push(
-        <div
-          className={`CollectBox CollectBox${i + 1}`}
-          key={i + 1}
-          style={{ transform: `translateX(${position}px)`, left: `${leftPosition}px` }}
-        >
-        </div>
-      );
-    }
-    return boxes;
+      console.log(value[0].top)
+      for (let i = 0; i < value.length; i++) {
+        
+        // 베열에서 db에서 받은 값의 인덱스 찾기
+        const topIndex = topClothesName.findIndex(item => item === value[i].top);
+        const bottomIndex = bottomClothesName.findIndex(item => item === value[i].bottom);
+        const setIndex = setClothesName.findIndex(item => item === value[i].set);
+        const accessorieIndex = accessorieClothesName.findIndex(item => item === value[i].accessorie);
+        const shoesIndex = shoesClothesName.findIndex(item => item === value[0].shoes);
+       
+        const leftPosition = 300 + (i - Math.floor(position / 1400)) * 1550;
+        boxes.push(
+          <div
+            className={`CollectBox CollectBox${i + 1}`}
+            key={i + 1}
+            style={{ transform: `translateX(${position}px)`, left: `${leftPosition}px` }}
+          >
+
+            {/* 캐릭터 */}
+            <div className="CharacterScreen">
+              <img className="character-co" src={`/images/character.png`} alt="" />
+              <img
+                style={{ position: 'absolute', top: Toplocation[topIndex] }}
+                src={`/images/cloths/${value[i].top}`}
+                alt="상의 이미지"
+                className='top-co'
+              />
+              <img
+                style={{ position: 'absolute', left: bottomLeftlocation[bottomIndex], bottom: Bottomlocation[bottomIndex] }}
+                src={`/images/cloths/${value[i].bottom}`}
+                alt="하의 이미지"
+                className='bottom-co'
+              />
+              <img style={{ position: 'absolute', top: setlocation[setIndex] }}
+                src={`/images/cloths/${value[i].set}`}
+                alt="세트 이미지"
+                className='set-co'
+              />
+              <img style={{ position: 'absolute', top: accessorieTopLocation[accessorieIndex], left: accessorieLeftLocation[accessorieIndex] }}
+                src={`/images/cloths/${value[i].accessorie}`}
+                alt="악세사리"
+                className='accessorie-co'
+              />
+              <img style={{ position: 'absolute', top: shoesTopLocation[shoesIndex] }}
+                src={`/images/cloths/${value[i].shoes}`}
+                alt="신발"
+                className='shoes-co'
+              />
+            </div>
+          </div>
+        );
+      }
+      return boxes;
     }
   };
 
